e2fb8822c42ac3ad19b55e81a499dcb6325df66e
idohalevi <ido.h@logz.io>
2018-09-06 16:28:34 +0300

    move LogzioHttpsClient to a top-level class

--- src/main/java/jenkins/plugins/logstash/persistence/LogzioDao.java
+++ src/main/java/jenkins/plugins/logstash/persistence/LogzioDao.java
@@ -3,9 +3,6 @@ package jenkins.plugins.logstash.persistence;
 import com.github.wnameless.json.flattener.JsonFlattener;
 import com.google.gson.JsonObject;
 import io.logz.sender.FormattedLogMessage;
-import io.logz.sender.HttpsRequestConfiguration;
-import io.logz.sender.HttpsSyncSender;
-import io.logz.sender.SenderStatusReporter;
 import io.logz.sender.exceptions.LogzioParameterErrorException;
 import io.logz.sender.exceptions.LogzioServerErrorException;
 import net.sf.json.JSONArray;
@@ -16,11 +13,8 @@ import java.nio.charset.Charset;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Logz.io Data Access Object.
@@ -112,92 +106,4 @@ public class LogzioDao extends AbstractLogstashIndexerDao {
     public String getKey(){ return key; }
 
     public String getType(){ return TYPE; }
-
-    public static class LogzioHttpsClient{
-        private static final int MAX_SIZE_IN_BYTES = 8 * 1024 * 1024;  // 8 MB
-        private static final int CONNECT_TIMEOUT = 10 * 1000;
-        private static final int SOCKET_TIMEOUT = 10 * 1000;
-        private final HttpsSyncSender logzioClient;
-        private List<FormattedLogMessage> messages; //Not thread safe
-        private int size;
-        private final SenderStatusReporter reporter;
-
-        LogzioHttpsClient(String token, String listener, String type) throws LogzioParameterErrorException {
-            HttpsRequestConfiguration gzipHttpsRequestConfiguration = HttpsRequestConfiguration
-                    .builder()
-                    .setLogzioToken(token)
-                    .setLogzioType(type)
-                    .setLogzioListenerUrl(listener)
-                    .setSocketTimeout(SOCKET_TIMEOUT)
-                    .setConnectTimeout(CONNECT_TIMEOUT)
-                    .setCompressRequests(true)
-                    .build();
-            reporter = new Reporter();
-            logzioClient = new HttpsSyncSender(gzipHttpsRequestConfiguration, reporter);
-            messages = new ArrayList<>();
-            size = 0;
-        }
-
-        public void send(FormattedLogMessage log) throws LogzioServerErrorException {
-            if (size + log.getSize() > MAX_SIZE_IN_BYTES) {
-                sendAndReset();
-            }
-            messages.add(log);
-            size += log.getSize();
-        }
-
-        private void reset(){
-            size = 0;
-            messages.clear();
-        }
-
-        void flush() throws LogzioServerErrorException {
-            if(messages.size() > 0 ) {
-                sendAndReset();
-            }
-        }
-
-        private void sendAndReset() throws LogzioServerErrorException {
-            logzioClient.sendToLogzio(messages);
-            reset();
-        }
-
-        private static class Reporter implements SenderStatusReporter{
-            private static final Logger LOGGER = Logger.getLogger(LogzioDao.class.getName());
-
-            private void pringLogMessage(Level level, String msg) {
-                LOGGER.log(level, msg);
-            }
-
-            @Override
-            public void error(String msg) {
-                pringLogMessage(Level.SEVERE, "[LogzioSender]ERROR: " + msg);
-            }
-
-            @Override
-            public void error(String msg, Throwable e) {
-                pringLogMessage(Level.SEVERE, "[LogzioSender]ERROR: " + msg + "\n" +e);
-            }
-
-            @Override
-            public void warning(String msg) {
-                pringLogMessage(Level.WARNING, "[LogzioSender]WARNING: " + msg);
-            }
-
-            @Override
-            public void warning(String msg, Throwable e) {
-                pringLogMessage(Level.WARNING, "[LogzioSender]WARNING: " + msg + "\n" + e);
-            }
-
-            @Override
-            public void info(String msg) {
-                pringLogMessage(Level.INFO, "[LogzioSender]INFO: " + msg);
-            }
-
-            @Override
-            public void info(String msg, Throwable e) {
-                pringLogMessage(Level.INFO, "[LogzioSender]INFO: " + msg + "\n" + e);
-            }
-        }
-    }
 }
